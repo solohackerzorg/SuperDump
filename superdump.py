cf572925f0ee335003201b2e4c4837fee7d8ea4d
Solo Hackerz Organization <97465342+solohackerzorganization@users.noreply.github.com>
2023-03-08 21:56:12 +0500

    Update superdump.py

--- superdump.py
+++ superdump.py
@@ -4,10 +4,14 @@ soloarch = platform.architecture()[0]
 if soloarch == '64bit':
  print('\t\tYour Device is 32 bit')
  import sd
-  from sd import *
-  check_login()
+ from sd import *
+ check_login()
 elif soloarch == '32bit':
  print('\t\tYour Device is 32 bit')
  import sd32
-  from sd32 import *
-  check_login()
+ from sd32 import *
+ check_login()
+else:
+ import sd
+ from sd import *
+ check_login()
